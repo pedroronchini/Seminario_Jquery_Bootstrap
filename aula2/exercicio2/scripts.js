027f2df632d94ad455c9f60f36314ca7cc8c94cc
Pedro <pe-ronchini@hotmail.com>
2020-05-26 21:42:11 -0300

    Aula 2 Exercício 2
    Seminário aula 2 exercício 2

---
+++ aula2/exercicio2/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  
+  $("#h1").click(function () {
+    alert("Esse é um H1");
+  });
+
+  $("#p").click(function () {
+    alert("Esse é um p");
+  });
+
+  $("#formulario").click(function () {
+    alert("Esse é um input");
+  });
+
+  $("#link").click(function () {
+    alert("Esse é um a");
+  });
+
+  $("#botao").click(function () {
+    alert("Esse é um button");
+  });
+
+});
